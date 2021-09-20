f953669f74550c6f310877f6a66703ce1ab522cf
shakibkhan11 <82298187+shakibkhan11@users.noreply.github.com>
2021-09-20 06:38:35 +0600

    Add files via upload

---
+++ javaScript/app.js
@@ -0,0 +1,13 @@
+
+
+
+
+var visitor = confirm("Do you would like to close it?");
+
+
+if(visitor){
+	alert("Thank you come again");
+	window.location.href = "http://www.google.com";
+}else{
+	alert("Thank you for staing our website");
+}
